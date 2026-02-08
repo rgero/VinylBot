79f318f5a23e099a560f993e00d8bc5bbfef4e24
Roy Gero <roy.gero.ii@gmail.com>
2026-02-08 13:15:05 -0500

    Correcting the help commands

--- VinylBot/src/discord/ProcessHelp.ts
+++ VinylBot/src/discord/ProcessHelp.ts
@@ -12,25 +12,26 @@ export const ProcessHelp = async (message: Message) => {
           name: "Collection Management", 
           value: 
             "`!want {spotify link}`: Adds an album to the want list.\n" +
-            "`!have {spotify link}`: Adds an album to the have list.\n" +
             "`!wantlist`: Gives you the whole want list.\n" +
             "`!wantlist {person}`: Gives you the want list of that person.\n" +
             "`!wantlist {search term}`: Gives you the want list items that match that term.\n" +
-            "`!havelist`: Gives you the whole have list.\n" +
-            "`!havelist {person}`: Gives you the have list of that person.\n" +
-            "`!havelist {search term}`: Gives you the have list items that match that term."
+            "`!have`: Gives you the whole have list.\n" +
+            "`!have {person}`: Gives you the have list of that person.\n" +
+            "`!have {search term}`: Gives you the have list items that match that term."
         },
         { 
           name: "Tracking Playback", 
           value: 
-            "`!play {spotify link}`: Adds a play for that album.\n" +
-            "`!play {artist OR album}`: Adds a play for that album. If there is more than one result, it will give you the drop down." 
+            "`!play {spotify link} {user}`: Adds a play for that album.\n" +
+            "`!play {artist OR album} {user}`: Adds a play for that album. If there is more than one result, it will give you the drop down.\n" +
+            "The user mention is to include multiple listeners" 
         },
         { 
           name: "Discovery & Stats", 
           value: 
             "`!random`: Chooses a random vinyl.\n" +
             "`!random {person}`: Chooses a random vinyl liked by that person.\n" +
+            "`!random {term}`: Chooses a random vinyl based on that term.\n" +
             "`!random store`: Chooses a random store.\n" +
             "`!info {album name}`: Gives you some info about the album.\n" +
             "`!top {user}`: Returns top artists by album count (household if no user specified).\n" +
